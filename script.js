0e264bd21831b0ef663b421c7e0b7076cfb26efb
TheBetterSamuel <s10203319@connect.np.edu.sg>
2021-02-05 00:55:49 +0800

    added API

--- script.js
+++ script.js
@@ -2,4 +2,16 @@ $(document).ready(function(){
     $(".mainpagebutton").click(function(){
         window.location.href = "https://en.hololive.tv/";
     })
+    let url = "https://hololive-f999.restdb.io/rest/holoinfo"
+    let APIKEY = "600504ba1346a1524ff12b9b"
+    fetch(url,{
+        "method": "GET",
+        headers:{
+            "x-apikey": APIKEY
+        }
+    })
+    .then(Response => Response.json())
+    .then(data => {
+        console.log(data);
+    })
 })
